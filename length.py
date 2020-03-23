6997773b65a9a43d8a19dc7f4dd03b35d0992e76
fayiq farooqi <fayiqfarooqi@gmail.com>
2020-03-23 09:39:04 +0530

    deleting all files

--- length.py
+++
@@ -1,3 +0,0 @@
-# print(len("where there is a will there is a way"))
-x = "where there is a will, there is a way"
-print(len(x))
--- number of spaces.py
+++
@@ -1,6 +0,0 @@
-x = "where there is a will there is a way"
-s = 0
-for i in range(len(x)):
-    if x[i] == ' ':
-        s = s + 1
-print("there are",s,"spaces in this sentence")
